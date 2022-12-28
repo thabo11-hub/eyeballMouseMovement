ecab95ca8f6d9506fa9c55f28d1768665d3582c3
Thabo Sodi <thabosodi11@gmail.com>
2022-12-28 19:15:48 +0200

    get angle between the coordinates

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ document.addEventListener("mousemove", (e) => {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  const anchor = document.getElementById('anchor');
+  const anchor = document.getElementById("anchor");
   const rekt = anchor.getBoundingClientRect();
 
   const anchorX = rekt.left + rekt.width / 2;
   const anchorY = rekt.top + rekt.height / 2;
-
-  
 });
+
+function angle(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+
+  const rad = Math.atan2(dy, dx);
+  const deg = (rad * 180) / Math.PI;
+}
