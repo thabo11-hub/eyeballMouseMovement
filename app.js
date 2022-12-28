ea43b3d832e0d27ed3778e09251cc97afbb6ec27
Thabo Sodi <thabosodi11@gmail.com>
2022-12-28 19:10:31 +0200

    get mouse position

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-document.addEventListener('mousemove',(e) => {
-    
-})
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const anchor = document.getElementById('anchor');
+  const rekt = anchor.getBoundingClientRect();
+
+  const anchorX = rekt.left + rekt.width / 2;
+  const anchorY = rekt.top + rekt.height / 2;
+
+  
+});
