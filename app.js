45e8dcd26bc913d3febcd705e67c7dc1ef08a5a3
Thabo Sodi <thabosodi11@gmail.com>
2022-12-28 19:37:49 +0200

    move the calculations

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+const eyes = document.querySelectorAll(".eye");
+const anchor = document.getElementById("anchor");
+const rekt = anchor.getBoundingClientRect();
+
+const anchorX = rekt.left + rekt.width / 2;
+const anchorY = rekt.top + rekt.height / 2;
+
 document.addEventListener("mousemove", (e) => {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  const anchor = document.getElementById("anchor");
-  const rekt = anchor.getBoundingClientRect();
-
-  const anchorX = rekt.left + rekt.width / 2;
-  const anchorY = rekt.top + rekt.height / 2;
-
   const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
 
-  const eyes = document.querySelectorAll(".eye");
   eyes.forEach((eye) => {
     eye.style.transform = `rotate(${90 + angleDeg}deg)`;
     anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
